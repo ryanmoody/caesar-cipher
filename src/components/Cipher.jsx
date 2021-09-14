19dafc68d6c8e1978df3a831d331830aa5b34935
Ryan Moody <ryan@ryanmoodycodes.com>
2021-09-14 17:48:14 -0500

    frfeeature: update header

--- src/components/Cipher.jsx
+++ src/components/Cipher.jsx
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
-import { encode, decode } from '../util/cipher-util';
+import React, { useState } from "react";
+import { encode, decode } from "../util/cipher-util";
 
 const Cipher = () => {
-  const [plainText, setPlainText] = useState('');
-  const [cipherText, setCipherText] = useState('');
+  const [plainText, setPlainText] = useState("");
+  const [cipherText, setCipherText] = useState("");
   const [shiftValue, setShiftValue] = useState(3);
 
   const onEncode = () => {
@@ -25,61 +25,65 @@ const Cipher = () => {
   };
 
   const clearPlainText = () => {
-    setPlainText('');
+    setPlainText("");
   };
 
   const clearCipherText = () => {
-    setCipherText('');
+    setCipherText("");
   };
-  
+
   return (
-    <div className='cipher-container'>
-      <div className="instructions-container">
-        <h2 className="accent-color">Instructions:</h2>
-        <p>Change the shift amount by pressing the <span className="accent-color">-</span>/<span className="accent-color">+</span> buttons.</p>
-        <p>Encrypt a message by submitting some text in the <span className="accent-color">Plaintext</span> box.</p>
-        <p>Decrypt a message by submitting some text in the <span className="accent-color">Ciphertext</span> box.</p>
-      </div>
-      <div className='input-container shadow'>
+    <div className="cipher-container">
+      <div className="input-container shadow">
         <div className="flex">
           <p>Plaintext</p>
-          <button className='clear-button' onClick={clearPlainText}>clear</button>
+          <button className="clear-button" onClick={clearPlainText}>
+            clear
+          </button>
         </div>
         <textarea
-          placeholder={`Enter some text...`}
+          placeholder={`But, for mine own part, it was Greek to me...`}
           onChange={(event) => setPlainText(event.target.value)}
           value={plainText}
-          name='plain-text'
-          maxLength='500'
-          rows='8'
-          wrap='soft'
-        >
-        </textarea>
-        <button className='button' onClick={onEncode}>Encrypt</button>
+          name="plain-text"
+          maxLength="500"
+          rows="8"
+          wrap="soft"
+        ></textarea>
+        <button className="button" onClick={onEncode}>
+          Encrypt
+        </button>
       </div>
-      <div className='controls-group'>
-        <button className='counter-button' onClick={onShiftDown}>-</button>
-        <div className='counter-number'>
-          <p className='counter-text'>{shiftValue}</p>
+      <div className="controls-group">
+        <button className="counter-button" onClick={onShiftDown}>
+          -
+        </button>
+        <div className="counter-number">
+          <p className="counter-text">{shiftValue}</p>
         </div>
-        <button className='counter-button' onClick={onShiftUp}>+</button>
+        <button className="counter-button" onClick={onShiftUp}>
+          +
+        </button>
       </div>
-      <div className='input-container shadow'>
+      <div className="input-container shadow">
         <div className="flex">
           <p>Ciphertext</p>
-          <button className='clear-button' onClick={clearCipherText}>clear</button>
+          <button className="clear-button" onClick={clearCipherText}>
+            clear
+          </button>
         </div>
         <textarea
-          placeholder={`Enter some text...`}
+          placeholder={`Exw, iru plqh rzq sduw, lw zdv Juhhn wr ph...`}
           onChange={(event) => setCipherText(event.target.value)}
           value={cipherText}
-          name='cipher-text'
-          maxLength='500'
-          rows='8'
-          wrap='soft'
-        >
-        </textarea>
-        <button className='button' onClick={onDecode}>Decrypt</button>
+          name="cipher-text"
+          maxLength="500"
+          rows="8"
+          wrap="soft"
+        ></textarea>
+        <button className="button" onClick={onDecode}>
+          Decrypt
+        </button>
       </div>
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
-import '../App.css';
+import React from "react";
+import { FaGithub } from "react-icons/fa";
+import { GiLaurels } from "react-icons/gi";
+import "../App.css";
 
 const Header = () => {
   return (
-    <header className='header'>
-      <h1 className='logo'>Caesar Cipher</h1>
+    <header className="header">
+      <div className="logo">
+        <GiLaurels className="logo-icon" size="2rem" />
+        <h1 className="logo-text">Caesar Cipher</h1>
+      </div>
+      <a
+        href="https://github.com/ryanmoody/caesar-cipher"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FaGithub className="github-icon" size="2rem" title="GitHub" />
+      </a>
     </header>
   );
 };
